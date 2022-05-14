cb08768b54e2f319803eb43a6ca3169bfe775391
David Saah <dasorange.hope@gmail.com>
2022-05-14 14:26:13 +0000

    deconstruct answers from `\`operation

--- building-stuff/linear-systems.jl
+++ building-stuff/linear-systems.jl
@@ -6,9 +6,19 @@ and three variables
 print("How many system of equations (number of variables): ")
 num = parse(Int64, readline())
 
+
 A = zeros(num, num)
 B = zeros(num)
-C = zeros(num)
+
+
+# function to round Vector to n decimal places
+function roundVector(A::Vector{Float64}, n::Int64)
+    for x in 1:length(A)
+        A[x] = round(A[x], digits=n)
+    end
+    return A
+end
+
 
 for x in 1:num
     println("Enter equation $x")
@@ -36,42 +46,17 @@ for x in 1:num
         A[x], A[num+x], A[2num+x] = a, b, c
         B[x] = d
     end
-
-    
-    # For 2x2 matrix
-    # 
-    # x = 1
-    # A[1], A[3] = a, b
-    # 3 = num + x
-    # x =  2
-    # A[2], A[4] = a, b
-    # 4 = num + x
-
-    # For 3x3 matrix
-    # num = 3
-    # x = 1
-    # A[1], A[4], A[7] = a, b, c
-    # 4 = num + x
-    # x =  2
-    # A[2], A[5], A[8] = a, b, c
-    # 8 = 2num + 2
-    # x = 3
-    # A[3], A[6], A[9] = a, b, c
-    # 
 end
 
-display(A)
-println("")
-display(B)
+println("-"^30)
 
-
-# # function to round Vector to n decimal places
-# function roundVector(A::Vector{Float64}, n::Int64)
-#     for x in 1:length(A)
-#         A[x] = round(A[x], digits=n)
-#     end
-#     return A
-# end
-
-
-# display(roundVector(A, 4))
+if num == 2
+    x, y = roundVector(A\B, 4)
+    println("x: $x")
+    println("y: $y")
+elseif num == 3
+    x, y, z = roundVector(A\B, 4)
+    println("x: $x")
+    println("y: $y")
+    println("z: $z")
+end
